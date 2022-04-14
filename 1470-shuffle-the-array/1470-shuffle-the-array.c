0d6c7385a22ab70634314427bf21764be6c9a66a
Emre <64619512+OsmanEmreYilmaz@users.noreply.github.com>
2022-04-14 17:45:04 +0300

    Time: 15 ms (67.33%), Space: 7 MB (74.50%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.c
@@ -0,0 +1,19 @@
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* shuffle(int* nums, int numsSize, int n, int* returnSize){
+     *returnSize = numsSize;
+    int* ans = (int*) malloc(numsSize * sizeof(int));
+    int k=0;
+    for(int i=0;i<numsSize;i++){
+        if(i%2==0){
+            ans[i]=nums[k++];
+        }
+        else{
+            ans[i]=nums[n++];
+        }
+    }
+    return ans;
+}
