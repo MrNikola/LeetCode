a1a1a03fbd15d3e3666a697979b8a0cf7b3b682d
Emre <64619512+OsmanEmreYilmaz@users.noreply.github.com>
2022-05-11 23:33:41 +0300

    Time: 31 ms, Memory: 7.2 MB - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.c
@@ -0,0 +1,13 @@
+
+
+bool validMountainArray(int* arr, int arrSize){
+
+   int i=0;
+    while(i<arrSize-1 && arr[i]<arr[i+1])
+        i++;
+    if(i==0 || i+1==arrSize)
+        return false;
+    while(i<arrSize-1 && arr[i]>arr[i+1])
+        i++;
+    return i == arrSize - 1;
+}
