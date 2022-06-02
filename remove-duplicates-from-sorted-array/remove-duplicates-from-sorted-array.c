6cff0cc7a01d50603166abcd8f2927a312194a9c
Emre <64619512+OsmanEmreYilmaz@users.noreply.github.com>
2022-06-02 12:05:27 +0300

    Time: 19 ms, Memory: 7.6 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.c
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.c
@@ -2,22 +2,16 @@
 
 int removeDuplicates(int* nums, int numsSize){
 
-    int i,k=1;
-    for(i=0;i<numsSize-1;i++){
-        if(nums[i]<nums[i+1]){
-            nums[k]=nums[i+1];
-            k++;
+     int j=0;
+    int k=numsSize;
+    
+    for(int i=1; i<numsSize; i++){
+        if(nums[j]==nums[i]){
+            k--;
+        }
+        else{
+            nums[++j]=nums[i];
         }
     }
     return k;
 }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
